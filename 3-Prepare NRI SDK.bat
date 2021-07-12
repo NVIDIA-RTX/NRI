5cdc4a272800e9d423b2e622371548e222a28ba7
ifedorov <ifedorov@nvidia.com>
2021-07-12 11:17:45 +0300

    fixed batch scripts

--- 3-Prepare NRI SDK.bat
+++ 3-Prepare NRI SDK.bat
@@ -1,19 +1,23 @@
 @echo off
 
-set TARGET_VS=vs2017
-if exist "_Compiler\vs2019" set TARGET_VS=vs2019
-
 rd /q /s "_NRI_SDK"
+
 mkdir "_NRI_SDK"
 cd "_NRI_SDK"
+copy "..\LICENSE.txt" "."
+copy "..\README.md" "."
+
 mkdir "Lib"
+
 mkdir "Lib\Debug"
+copy "..\_Build\Debug\NRI.dll" "Lib\Debug"
+copy "..\_Build\Debug\NRI.lib" "Lib\Debug"
+copy "..\_Build\Debug\NRI.pdb" "Lib\Debug"
+
 mkdir "Lib\Release"
+copy "..\_Build\Release\NRI.dll" "Lib\Release"
+copy "..\_Build\Release\NRI.lib" "Lib\Release"
+copy "..\_Build\Release\NRI.pdb" "Lib\Release"
+
 mkdir "Include"
-mkdir "Include\Extensions"
-copy "..\_Build\%TARGET_VS%\Lib\Debug\NRI.lib" "Lib\Debug"
-copy "..\_Build\%TARGET_VS%\Lib\Release\NRI.lib" "Lib\Release"
-copy "..\_Build\%TARGET_VS%\Bin\Debug\NRI.*" "Lib\Debug"
-copy "..\_Build\%TARGET_VS%\Bin\Release\NRI.*" "Lib\Release"
-copy "..\NRI\Include\*" "Include"
-copy "..\NRI\Include\Extensions\*" "Include\Extensions"
+copy "..\Include\*" "Include"
