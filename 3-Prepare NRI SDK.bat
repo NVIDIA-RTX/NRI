e462c706212688142c320bc0333749a24d1c32c6
Dmitry Zhdan <dzhdan@nvidia.com>
2021-08-31 15:15:25 +0300

    Updated script preparing SDK

--- 3-Prepare NRI SDK.bat
+++ 3-Prepare NRI SDK.bat
@@ -1,25 +1,23 @@
 @echo off
 
-rd /q /s "_NRI_SDK"
+set NRI_DIR=.
 
 mkdir "_NRI_SDK"
 cd "_NRI_SDK"
-copy "..\LICENSE.txt" "."
-copy "..\README.md" "."
+
+copy "..\%NRI_DIR%\LICENSE.txt" "."
+
+mkdir "Include"
+copy "..\%NRI_DIR%\Include\*" "Include"
+
+mkdir "Include\Extensions"
+copy "..\%NRI_DIR%\Include\Extensions\*" "Include\Extensions"
 
 mkdir "Lib"
-
 mkdir "Lib\Debug"
 copy "..\_Build\Debug\NRI.dll" "Lib\Debug"
 copy "..\_Build\Debug\NRI.lib" "Lib\Debug"
 copy "..\_Build\Debug\NRI.pdb" "Lib\Debug"
-
 mkdir "Lib\Release"
 copy "..\_Build\Release\NRI.dll" "Lib\Release"
 copy "..\_Build\Release\NRI.lib" "Lib\Release"
-copy "..\_Build\Release\NRI.pdb" "Lib\Release"
-
-mkdir "Include"
-copy "..\Include\*" "Include"
-mkdir "Include\Extensions"
-copy "..\Include\Extensions\*" "Include\Extensions"
