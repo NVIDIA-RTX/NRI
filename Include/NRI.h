939f547520034d1ccdb01af6d36c49d30b57b13d
dzhdan <dzhdan@nvidia.com>
2023-10-09 11:50:28 +0800

    Fixed version number  (99=>98)

--- Include/NRI.h
+++ Include/NRI.h
@@ -14,7 +14,7 @@ license agreement from NVIDIA CORPORATION is strictly prohibited.
 #include <stddef.h>
 
 #define NRI_VERSION_MAJOR 1
-#define NRI_VERSION_MINOR 99
+#define NRI_VERSION_MINOR 98
 #define NRI_VERSION_DATE "9 October 2023"
 #define NRI_INTERFACE( name ) #name, sizeof(name)
 
