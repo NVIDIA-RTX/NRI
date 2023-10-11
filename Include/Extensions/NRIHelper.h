05a517d06d5dfab55a9a9edce4a428de6747e2c9
dzhdan <dzhdan@nvidia.com>
2023-10-11 16:14:51 +0800

    Polishing

--- Include/Extensions/NRIHelper.h
+++ Include/Extensions/NRIHelper.h
@@ -59,7 +59,7 @@ NRI_STRUCT(HelperInterface)
     NRI_NAME(Result) (NRI_CALL *WaitForIdle)(NRI_NAME_REF(CommandQueue) commandQueue);
 };
 
-static inline NRI_NAME(Format) NRI_NAME(GetSupportedDepthFormat)(const NRI_NAME_REF(CoreInterface) coreInterface, const NRI_NAME_REF(Device) device, uint32_t minBits, bool stencil)
+static inline NRI_NAME(Format) NRI_FUNC_NAME(GetSupportedDepthFormat)(const NRI_NAME_REF(CoreInterface) coreInterface, const NRI_NAME_REF(Device) device, uint32_t minBits, bool stencil)
 {
     if (stencil)
     {
@@ -92,7 +92,7 @@ static inline NRI_NAME(Format) NRI_NAME(GetSupportedDepthFormat)(const NRI_NAME_
     return NRI_ENUM_MEMBER(Format, UNKNOWN);
 }
 
-static inline NRI_NAME(TextureDesc) NRI_NAME(Texture1D)(NRI_NAME(Format) format, uint16_t width, uint16_t mipNum NRI_DEFAULT_VALUE(1), uint16_t arraySize NRI_DEFAULT_VALUE(1), NRI_NAME(TextureUsageBits) usageMask NRI_DEFAULT_VALUE(NRI_ENUM_MEMBER(TextureUsageBits, SHADER_RESOURCE)))
+static inline NRI_NAME(TextureDesc) NRI_FUNC_NAME(Texture1D)(NRI_NAME(Format) format, uint16_t width, uint16_t mipNum NRI_DEFAULT_VALUE(1), uint16_t arraySize NRI_DEFAULT_VALUE(1), NRI_NAME(TextureUsageBits) usageMask NRI_DEFAULT_VALUE(NRI_ENUM_MEMBER(TextureUsageBits, SHADER_RESOURCE)))
 {
     NRI_NAME(TextureDesc) textureDesc = NRI_ZERO_INIT;
     textureDesc.type = NRI_ENUM_MEMBER(TextureType, TEXTURE_1D);
@@ -108,7 +108,7 @@ static inline NRI_NAME(TextureDesc) NRI_NAME(Texture1D)(NRI_NAME(Format) format,
     return textureDesc;
 }
 
-static inline NRI_NAME(TextureDesc) NRI_NAME(Texture2D)(NRI_NAME(Format) format, uint16_t width, uint16_t height, uint16_t mipNum NRI_DEFAULT_VALUE(1), uint16_t arraySize NRI_DEFAULT_VALUE(1), NRI_NAME(TextureUsageBits) usageMask NRI_DEFAULT_VALUE(NRI_ENUM_MEMBER(TextureUsageBits, SHADER_RESOURCE)), uint8_t sampleNum NRI_DEFAULT_VALUE(1))
+static inline NRI_NAME(TextureDesc) NRI_FUNC_NAME(Texture2D)(NRI_NAME(Format) format, uint16_t width, uint16_t height, uint16_t mipNum NRI_DEFAULT_VALUE(1), uint16_t arraySize NRI_DEFAULT_VALUE(1), NRI_NAME(TextureUsageBits) usageMask NRI_DEFAULT_VALUE(NRI_ENUM_MEMBER(TextureUsageBits, SHADER_RESOURCE)), uint8_t sampleNum NRI_DEFAULT_VALUE(1))
 {
     NRI_NAME(TextureDesc) textureDesc = NRI_ZERO_INIT;
     textureDesc.type = NRI_ENUM_MEMBER(TextureType, TEXTURE_2D);
@@ -124,7 +124,7 @@ static inline NRI_NAME(TextureDesc) NRI_NAME(Texture2D)(NRI_NAME(Format) format,
     return textureDesc;
 }
 
-static inline NRI_NAME(TextureDesc) NRI_NAME(Texture3D)(NRI_NAME(Format) format, uint16_t width, uint16_t height, uint16_t depth, uint16_t mipNum NRI_DEFAULT_VALUE(1), NRI_NAME(TextureUsageBits) usageMask NRI_DEFAULT_VALUE(NRI_ENUM_MEMBER(TextureUsageBits, SHADER_RESOURCE)))
+static inline NRI_NAME(TextureDesc) NRI_FUNC_NAME(Texture3D)(NRI_NAME(Format) format, uint16_t width, uint16_t height, uint16_t depth, uint16_t mipNum NRI_DEFAULT_VALUE(1), NRI_NAME(TextureUsageBits) usageMask NRI_DEFAULT_VALUE(NRI_ENUM_MEMBER(TextureUsageBits, SHADER_RESOURCE)))
 {
     NRI_NAME(TextureDesc) textureDesc = NRI_ZERO_INIT;
     textureDesc.type = NRI_ENUM_MEMBER(TextureType, TEXTURE_3D);
@@ -140,7 +140,7 @@ static inline NRI_NAME(TextureDesc) NRI_NAME(Texture3D)(NRI_NAME(Format) format,
     return textureDesc;
 }
 
-static inline NRI_NAME(TextureTransitionBarrierDesc) NRI_NAME(TextureTransition)(NRI_NAME(Texture)* texture, NRI_NAME(AccessBits) prevAccess, NRI_NAME(AccessBits) nextAccess, NRI_NAME(TextureLayout) prevLayout, NRI_NAME(TextureLayout) nextLayout,
+static inline NRI_NAME(TextureTransitionBarrierDesc) NRI_FUNC_NAME(TextureTransition)(NRI_NAME(Texture)* texture, NRI_NAME(AccessBits) prevAccess, NRI_NAME(AccessBits) nextAccess, NRI_NAME(TextureLayout) prevLayout, NRI_NAME(TextureLayout) nextLayout,
     uint16_t mipOffset NRI_DEFAULT_VALUE(0), uint16_t mipNum NRI_DEFAULT_VALUE(NRI_NAME(REMAINING_MIP_LEVELS)), uint16_t arrayOffset NRI_DEFAULT_VALUE(0), uint16_t arraySize NRI_DEFAULT_VALUE(NRI_NAME(REMAINING_ARRAY_LAYERS)))
 {
     NRI_NAME(TextureTransitionBarrierDesc) textureTransitionBarrierDesc = NRI_ZERO_INIT;
@@ -157,7 +157,7 @@ static inline NRI_NAME(TextureTransitionBarrierDesc) NRI_NAME(TextureTransition)
     return textureTransitionBarrierDesc;
 }
 
-static inline NRI_NAME(TextureTransitionBarrierDesc) NRI_NAME(TextureTransitionFromUnknown)(NRI_NAME(Texture)* texture, NRI_NAME(AccessBits) nextAccess, NRI_NAME(TextureLayout) nextLayout,
+static inline NRI_NAME(TextureTransitionBarrierDesc) NRI_FUNC_NAME(TextureTransitionFromUnknown)(NRI_NAME(Texture)* texture, NRI_NAME(AccessBits) nextAccess, NRI_NAME(TextureLayout) nextLayout,
     uint16_t mipOffset NRI_DEFAULT_VALUE(0), uint16_t mipNum NRI_DEFAULT_VALUE(NRI_NAME(REMAINING_MIP_LEVELS)), uint16_t arrayOffset NRI_DEFAULT_VALUE(0), uint16_t arraySize NRI_DEFAULT_VALUE(NRI_NAME(REMAINING_ARRAY_LAYERS)))
 {
     NRI_NAME(TextureTransitionBarrierDesc) textureTransitionBarrierDesc = NRI_ZERO_INIT;
@@ -174,7 +174,7 @@ static inline NRI_NAME(TextureTransitionBarrierDesc) NRI_NAME(TextureTransitionF
     return textureTransitionBarrierDesc;
 }
 
-static inline NRI_NAME(TextureTransitionBarrierDesc) NRI_NAME(TextureTransitionFromState)(NRI_NAME_REF(TextureTransitionBarrierDesc) prevState, NRI_NAME(AccessBits) nextAccess, NRI_NAME(TextureLayout) nextLayout,
+static inline NRI_NAME(TextureTransitionBarrierDesc) NRI_FUNC_NAME(TextureTransitionFromState)(NRI_NAME_REF(TextureTransitionBarrierDesc) prevState, NRI_NAME(AccessBits) nextAccess, NRI_NAME(TextureLayout) nextLayout,
     uint16_t mipOffset NRI_DEFAULT_VALUE(0), uint16_t mipNum NRI_DEFAULT_VALUE(NRI_NAME(REMAINING_MIP_LEVELS)))
 {
     NRI_REF_ACCESS(prevState)->mipOffset = mipOffset;
