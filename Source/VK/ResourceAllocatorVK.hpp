300661209bbfe5e6d6686953fd8e9d443cb9a8b7
dzhdan <dzhdan@nvidia.com>
2025-02-19 14:03:51 +0800

    Fix for Linux

--- Source/VK/ResourceAllocatorVK.hpp
+++ Source/VK/ResourceAllocatorVK.hpp
@@ -2,10 +2,12 @@
 
 #if defined(__GNUC__)
 #    pragma GCC diagnostic push
-#    pragma GCC diagnostic ignored "-Wunused-parameter -Wunused-variable"
+#    pragma GCC diagnostic ignored "-Wunused-variable"
+#    pragma GCC diagnostic ignored "-Wunused-parameter"
 #elif defined(__clang__)
 #    pragma clang diagnostic push
-#    pragma clang diagnostic ignored "-Wunused-parameter -Wunused-variable"
+#    pragma clang diagnostic ignored "-Wunused-variable"
+#    pragma clang diagnostic ignored "-Wunused-parameter"
 #else
 #    pragma warning(push)
 #    pragma warning(disable : 4100) // unreferenced formal parameter
