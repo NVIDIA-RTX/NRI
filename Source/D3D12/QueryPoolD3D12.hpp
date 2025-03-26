af334a546173ad1ac776bff5e1ce172e77e0a5c9
dzhdan <dzhdan@nvidia.com>
2025-03-26 14:49:10 +0800

    D3D12: fixed query type for devices not supporting mesh shaders

--- Source/D3D12/QueryPoolD3D12.hpp
+++ Source/D3D12/QueryPoolD3D12.hpp
@@ -17,14 +17,17 @@ Result QueryPoolD3D12::Create(const QueryPoolDesc& queryPoolDesc) {
         desc.Type = D3D12_QUERY_HEAP_TYPE_OCCLUSION;
     } else if (queryPoolDesc.queryType == QueryType::PIPELINE_STATISTICS) {
 #ifdef NRI_ENABLE_AGILITY_SDK_SUPPORT
-        // Prerequisite: D3D12_FEATURE_D3D12_OPTIONS9
-        m_QuerySize = sizeof(D3D12_QUERY_DATA_PIPELINE_STATISTICS1);
-        m_QueryType = D3D12_QUERY_TYPE_PIPELINE_STATISTICS1;
-        desc.Type = D3D12_QUERY_HEAP_TYPE_PIPELINE_STATISTICS1;
-#else
-        m_QuerySize = sizeof(D3D12_QUERY_DATA_PIPELINE_STATISTICS);
-        m_QueryType = D3D12_QUERY_TYPE_PIPELINE_STATISTICS;
-        desc.Type = D3D12_QUERY_HEAP_TYPE_PIPELINE_STATISTICS;
+        if (m_Device.GetDesc().isMeshShaderPipelineStatsSupported) {
+            m_QuerySize = sizeof(D3D12_QUERY_DATA_PIPELINE_STATISTICS1);
+            m_QueryType = D3D12_QUERY_TYPE_PIPELINE_STATISTICS1;
+            desc.Type = D3D12_QUERY_HEAP_TYPE_PIPELINE_STATISTICS1;
+        } else {
+#endif
+            m_QuerySize = sizeof(D3D12_QUERY_DATA_PIPELINE_STATISTICS);
+            m_QueryType = D3D12_QUERY_TYPE_PIPELINE_STATISTICS;
+            desc.Type = D3D12_QUERY_HEAP_TYPE_PIPELINE_STATISTICS;
+#ifdef NRI_ENABLE_AGILITY_SDK_SUPPORT
+        }
 #endif
     } else if (queryPoolDesc.queryType == QueryType::ACCELERATION_STRUCTURE_SIZE) {
         m_QuerySize = sizeof(uint64_t);
