cdcf1578e26ad358cc23dac2d394ed68b33e0903
dzhdan <dzhdan@nvidia.com>
2026-05-05 14:58:34 +0800

    Updated version

--- Include/NRI.h
+++ Include/NRI.h
@@ -38,8 +38,8 @@ Implicit:
 
 #pragma once
 
-#define NRI_VERSION 179
-#define NRI_VERSION_DATE "6 April 2026"
+#define NRI_VERSION 180
+#define NRI_VERSION_DATE "5 May 2026"
 
 // C/C++ compatible interface (auto-selection or via "NRI_FORCE_C" macro)
 #include "NRIDescs.h"
