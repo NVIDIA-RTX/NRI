f8949678204b2b382f6a13ee7ad040bf08bd7016
dzhdan <dzhdan@nvidia.com>
2026-06-04 12:47:04 +0800

    Fixed formatting

--- Source/D3D12/VideoPictureD3D12.hpp
+++ Source/D3D12/VideoPictureD3D12.hpp
@@ -1,13 +1,13 @@
 // © 2026 NVIDIA Corporation
 
 NRI_INLINE Result VideoPictureD3D12::Create(const VideoPictureDesc& videoPictureDesc) {
-        if (!videoPictureDesc.texture)
-            return Result::INVALID_ARGUMENT;
+    if (!videoPictureDesc.texture)
+        return Result::INVALID_ARGUMENT;
 
-        m_Texture = (TextureD3D12*)videoPictureDesc.texture;
+    m_Texture = (TextureD3D12*)videoPictureDesc.texture;
 
-        const TextureDesc& textureDesc = m_Texture->GetDesc();
-        m_Subresource = GetSubresourceIndex(videoPictureDesc.layer, textureDesc.layerNum, 0, textureDesc.mipNum, PlaneBits::COLOR);
+    const TextureDesc& textureDesc = m_Texture->GetDesc();
+    m_Subresource = GetSubresourceIndex(videoPictureDesc.layer, textureDesc.layerNum, 0, textureDesc.mipNum, PlaneBits::COLOR);
 
-        return Result::SUCCESS;
-    }
+    return Result::SUCCESS;
+}
